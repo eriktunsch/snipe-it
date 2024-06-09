bb18e7cd142463e223366d99b5864ec0c3914078
eriktunsch <eriktunsch@dk7et.de>
2024-06-09 04:52:10 +0200

    Update LoginController.php
    test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -125,7 +125,8 @@ class LoginController extends Controller
                     \Log::debug("Creating SAML user '$username'.");
 
                     $user = new User();
-                    \Log::debug(json_encode($saml->getAttributesWithFriendlyName()));
+                    \Log::debug(json_encode($saml->getAttributes()));
+                    \Log::debug(json_encode($saml->getAttributes()["http:\/\/schemas.xmlsoap.org\/ws\/2005\/05\/identity\/claims\/name"]));
                     $user->first_name = explode($saml->getAttributes()["name"])[0];
                     $user->last_name = explode($saml->getAttributes()["name"])[1];
                     $user->username = $username;
